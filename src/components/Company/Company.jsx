5164e8db2d5a5340da7a2cced8bf48a72c401efe
rbriday <ridaya100@gmail.com>
2025-09-21 02:42:00 +0600

    add responsinve

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -3,13 +3,13 @@ import Container from "../../Layout/Container";
 
 const Company = () => {
     return (
-        <div className="py-[136px] bg-[#F0F0F0]">
+        <div className="py-[50px] md:py-[136px] bg-[#F0F0F0]">
             <Container>
-                <div className="flex">
-                <div className="w-[413px] bg-orange py-[100px] px-[75px]">
-                    <h3 className="w-[262px] font-poppins font-bold text-[36px] text-white">Learn more about our company</h3>
+                <div className="md:flex">
+                <div className="w-auto md:w-[413px] bg-orange py-[50px] md:py-[100px] px-[20px] md:px-[75px]">
+                    <h3 className="w-auto md:w-[262px] font-poppins font-bold text-[25px] md:text-[36px] text-white">Learn more about our company</h3>
                 </div>
-                <div className="bg-[url(./assets/company.png)] bg-cover bg-no-repeat bg-center py-[200px] px-[282px]">
+                <div className="bg-[url(./assets/company.png)] bg-cover bg-no-repeat bg-center py-[100px] md:py-[200px] px-[50px] md:px-[282px]">
                 {/* <button className="font-poppins font-semibold text-[16px] text-orange py-[14px] px-[30px] bg-white cursor-pointer">Learn More</button> */}
                 <button class="btn relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-white text-orange hover:bg-orange group py-[13px] px-[30px] cursor-pointer">
                 <span class="w-56 h-48  bg-orange absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,21 +1,21 @@
 const Service = () => {
   return (
     <div className="mt-[30px]">
-      <div className="flex">
-        <div className="w-[50%] flex justify-end pr-[60px]">
-          <div className="w-[509px] pt-[106px]">
-            <h3 className="font-poppins font-bold text-[64px] text-primary">
+      <div className="md:flex">
+        <div className="w-auto md:w-[50%] md:flex justify-end pr-[60px]">
+          <div className=" mx-[20px] md:mx-0 w-auto md:w-[509px] md:pt-[106px] mb-[30px] md:mb-0">
+            <h3 className="font-poppins font-bold text-[30px] md:text-[64px] text-primary">
               Our Services
             </h3>
-            <p className="w-[405px] font-poppins font-medium text-[16px] text-secondary">
+            <p className="w-auto md:w-[405px] font-poppins font-medium text-[12px] md:text-[16px] text-secondary">
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout.
             </p>
           </div>
         </div>
-        <div className="w-[50%]">
+        <div className="w-auto md:w-[50%]">
           <div className="relative bg-[url(./assets/serviceOne.png)] bg-cover bg-no-repeat bg-center after:absolute after:content-[''] after:top-0 after:left-0 after:w-full after:h-full after:bg-black/60 z-[-1]">
-            <div className="relative z-[1] pt-[147px] pl-[115px] pb-[140px]">
+            <div className="relative z-[1] pt-[50px] md:pt-[147px] pl-[30px] md:pl-[115px] pb-[40px] md:pb-[140px]">
               <h3 className="font-poppins font-bold text-[30px] text-white mb-[19px]">
                 Modern natural oil and gas refineries.
               </h3>
@@ -26,10 +26,10 @@ const Service = () => {
           </div>
         </div>
       </div>
-      <div className="flex">
-        <div className="w-[50%]">
+      <div className="md:flex">
+        <div className="w-auto md:w-[50%]">
           <div className="relative bg-[url(./assets/serviceTwo.png)] bg-cover bg-no-repeat bg-center after:absolute after:content-[''] after:top-0 after:left-0 after:w-full after:h-full after:bg-black/60 z-[-1]">
-            <div className="relative z-[1] pt-[147px] pl-[115px] pb-[140px]">
+            <div className="relative z-[1] pt-[50px] md:pt-[147px] pl-[40px] md:pl-[115px] pb-[40px] md:pb-[140px]">
               <h3 className="font-poppins font-bold text-[30px] text-white mb-[19px]">
                 Supply of national industries.
               </h3>
@@ -39,9 +39,9 @@ const Service = () => {
             </div>
           </div>
         </div>
-        <div className="w-[50%]">
+        <div className="w-auto md:w-[50%]">
           <div className="relative bg-[url(./assets/serviceThree.png)] bg-cover bg-no-repeat bg-center after:absolute after:content-[''] after:top-0 after:left-0 after:w-full after:h-full after:bg-black/60 z-[-1]">
-            <div className="relative z-[1] pt-[147px] pl-[115px] pb-[140px]">
+            <div className="relative z-[1] pt-[50px] md:pt-[147px] pl-[50px] md:pl-[115px] pb-[40px] md:pb-[140px]">
               <h3 className="font-poppins font-bold text-[30px] text-white mb-[19px]">
                 National fuel distribution and supply.
               </h3>
